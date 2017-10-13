77f4d5504570a2c3acb677dbf186296742e13584
Unknown <cristhian.porras30@hotmail.com>
2017-10-12 18:29:05 -0600

    capas hechas

---
+++ Datos/Interface/clsInterfaceDatos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Interface
+{
+    public interface clsInterfaceDatos<T>
+    {
+        void Agregar_D(T obj);
+        void Modificar_D(T obj);
+        void Eliminar_D(T obj);
+        IEnumerable<T> devolverLISTA_D();
+        T buscarporID(int id);
+
+    }
+}
---
+++ Logica/Interface/clsInterfaceLogica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Interface
+{
+    public interface clsInterfaceLogica<T>
+    {
+        void Agregar_L(T obj);
+        void Modificar_L(T obj);
+        void Eliminar_L(T obj);
+        IEnumerable<T> devolverLISTA_L();
+        T buscarporID_L(int id);
+    }
+}
